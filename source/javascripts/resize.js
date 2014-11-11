cd15a31b7f3d2ddaba002a1517de16c29936dcd6
timsvoice <timsethvoice@gmail.com>
2014-11-11 00:27:15 -0500

    added marketing and updated splash

--- source/javascripts/resize.js
+++ source/javascripts/resize.js
@@ -1,7 +1,7 @@
 //splash 
 function resizesplash(divId) {
       var heights = window.innerHeight;
-      document.getElementById("splash").style.height = heights +0 + "px";
+      document.getElementById("splash").style.height = heights -14 + "px";
   }
 
   resizesplash();
